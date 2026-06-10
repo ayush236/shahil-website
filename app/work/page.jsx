78fa21cc96e29632d6564d5174c45411fedd4120
Adarsh Rana <codewithtopendra@gmail.com>
2026-06-10 17:29:38 +0545

    kkk

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='flex items-center justify-center w-full h-screen'>
         <h1>Work Page</h1>
     </div>
   )
